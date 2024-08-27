2fd71ad3feb87a674e15dc5db35635ad689dcbb5
anmol-srivastav_groww <img_2018012@iiitm.ac.in>
2024-08-28 03:51:20 +0530

    search bar and constants

---
+++ app/src/main/java/com/example/bookmymovie/presentation/ui/movielistscreen/components/SearchBar.kt
@@ -0,0 +1,59 @@
+package com.example.bookmymovie.presentation.ui.movielistscreen.components
+
+import androidx.compose.foundation.border
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.unit.dp
+import com.example.bookmymovie.utils.Constants
+
+@Composable
+fun SearchBar(
+    query: String,
+    onQueryChanged: (String) -> Unit,
+) {
+    val borderColor = if (isSystemInDarkTheme()) Color.White else Color.Gray
+
+    TextField(
+        value = query,
+        onValueChange = onQueryChanged,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+            .border(1.dp, MaterialTheme.colorScheme.tertiaryContainer,  RoundedCornerShape(8.dp)),
+        placeholder = {
+            Text(text = Constants.SEARCH_HINT)
+        },
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Default.Search,
+                contentDescription = Constants.SEARCH_ICON
+            )
+        },
+        enabled = true,
+        shape = RoundedCornerShape(topEnd = 8.dp, topStart = 8.dp, bottomEnd = 8.dp,
+            bottomStart = 8.dp),
+        singleLine = true,
+        keyboardOptions = KeyboardOptions.Default.copy(
+            imeAction = ImeAction.Search
+        ),
+        colors = TextFieldDefaults.colors(
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent,
+            disabledIndicatorColor = Color.Transparent
+        ),
+    )
+}
---
+++ app/src/main/java/com/example/bookmymovie/utils/Constants.kt
@@ -0,0 +1,12 @@
+package com.example.bookmymovie.utils
+
+object Constants {
+    const val DEFAULT_IMG_URL = "https://m.media-amazon.com/images/M/MV5BMTY3NjQwNDMwNV5BMl5BanBnXkFtZTcwNjE3ODgyMQ@@._V1_SX300.jpg"
+    const val LOADING = "Loading..."
+    const val NO_RESULTS = "No Results Found"
+    const val ERROR = "Error"
+    const val SEARCH_HINT = "Search Movies"
+    const val SEARCH_ICON = "search"
+    const val BACK_ICON = "back"
+    const val MOVIE_POSTER = "movie poster"
+}
